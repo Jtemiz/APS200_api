49f4b2382342f684269aeb86a38929d09c76a81f
Jtemiz <Joel.temiz@unibw.de>
2023-03-12 18:52:44 +0100

    changed sql commands.sql

--- commands.sql
+++ commands.sql
@@ -1,34 +1,39 @@
-DROP TABLE IF EXISTS `metadata`;
+create database aps200;
+create user 'aps200'@'%' identified by 'Jockel01.';
+grant all privileges on *.* to 'aps200'@'%';
+flush privileges;
 
-create table metadata
+create table if not exists ExampleTable
+(
+    POSITION        float(11, 3) null,
+    HOEHE           float(6, 3)  not null,
+    GESCHWINDIGKEIT float(6, 3)  null,
+    BREITE          int          null,
+    GRENZWERT       int          null
+);
+
+create table if not exists commentBtns
+(
+    comment varchar(10000) null
+);
+
+create table if not exists comments
+(
+    measurement varchar(20)    not null,
+    position    float(10, 2)   not null,
+    comment     varchar(10000) not null,
+    primary key (measurement, position)
+);
+
+create table if not exists metadata
 (
     measurement     varchar(18)  not null
         primary key,
-    location        text,
+    location        text         null,
     distance        float(20, 2) not null,
-    username        text,
-    measurementname text,
-    notes text
+    username        text         null,
+    measurementname text         null,
+    notes           text         null
 );
 
 
-
-CREATE TABLE IF NOT EXISTS `ExampleTable` (
-  `IDX` int(8) NOT NULL,
-  `POSITION` float(10,2) DEFAULT NULL,
-  `HOEHE` float(5,2) NOT NULL,
-  `GESCHWINDIGKEIT` float(5,2) DEFAULT NULL,
-  `BREITE` int(4) DEFAULT NULL,
-  `GRENZWERT` int(4) DEFAULT NULL
-);
-
-CREATE TABLE `comments` (
-  `measurement` varchar(20) NOT NULL,
-  `position` float(10,2) NOT NULL,
-  `comment` varchar(30) NOT NULL,
-  PRIMARY KEY (`measurement`,`position`)
-);
-
-CREATE TABLE `commentBtns` (
-  `comment` varchar(30) DEFAULT NULL
-);
